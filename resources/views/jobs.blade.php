df2ae5d857fa5c48595fe87405b3f8734fbcf652
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2024-03-22 18:11:46 +0000

    Fix missing attribute

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,9 +5,9 @@
     
     <ul>
         @foreach ($jobs as $job)
-        <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:text-red-500">
-            <li><strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }}</li>
-        </a>
+            <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:text-red-500">
+                <li><strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }}</li>
+            </a>
         @endforeach
     </ul>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,24 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+$jobs =  [
+    [
+        'id' => 1,
+        'title' => 'Director',
+        'salary' => '$50,000'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Programmer',
+        'salary' => '$70,000'
+    ],
+    [
+        'id' => 3,
+        'title' => 'Teacher',
+        'salary' => '$50,000'
+    ],
+];
+
 Route::get('/', function () {
     return view('home');
 });
@@ -15,46 +33,13 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function () use ($jobs) {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$70,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$50,000'
-            ],
-        ]
+        'jobs' => $jobs
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs =  [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$70,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$50,000'
-        ],
-    ];
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
 
     // $job = \Illuminate\support\Arr::first($jobs, function ($job) use ($id) {
     //     return $job['id'] === $id;
