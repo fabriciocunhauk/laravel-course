a128d7db325763eebe3be48a6e4f444da4bd3fd1
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2024-03-25 16:24:29 +0000

    Complete day 9

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,40 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return  [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$70,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$50,000'
-            ],
-        ];
-    }
+    protected $table = 'job_listings';
 
-    public static function find(int $id): array
-    {
-        $job =
-            Arr::first(Job::all(), fn ($job) => $job['id'] == $id);
-
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
