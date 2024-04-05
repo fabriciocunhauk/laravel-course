e15b7a65f50c558e7bfd3b3c1684ea016670874a
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2024-04-05 17:01:25 +0100

    Eager Loading and the N+1 Problem

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,16 +2,20 @@
     <x-slot:heading>
         Jobs Page
     </x-slot:heading>
-    
-    <ul>
+
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:text-red-500">
-                <li>
-                    <strong>
-                        {{ $job['title'] }}
-                    </strong>: Pays {{ $job['salary'] }}
-                </li>
-            </a>
+        <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+            <div class="font-bold text-blue-500">
+                {{ $job->employer->name}}
+            </div>
+            <div>
+                <strong>
+                    {{ $job['title'] }}
+                </strong>: Pays {{ $job['salary'] }}
+            </div>
+        </a>
         @endforeach
-    </ul>
+
+    </div>
 </x-layout>
