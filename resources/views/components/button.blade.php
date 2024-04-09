ef9f3b40943a26dd792a7fa1d60f57d96981d17d
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2024-04-09 16:24:24 +0100

    Always Validate. Never Trust the User.

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,1 @@
+ <a {{ $attributes->merge(['class' => "rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-600 hover:text-white outline outline-1 hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"])}} >{{ $slot }}</a>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,7 +23,6 @@
                                 <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
                                 <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
                                 <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
-                                <x-nav-link href="/jobs/create" :active="request()->is('jobs/create')">Create Job</x-nav-link>
                                 <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
                             </div>
                         </div>
@@ -91,9 +90,7 @@
                     </x-nav-link>
                     <x-nav-link href="/contact" :active="request()->is('contact')">
                         Contact
-                        Create Job
                     </x-nav-link>
-                    <x-nav-link href="/jobs/create" :active="request()->is('jobs/create')">Create Job</x-nav-link>
                     <x-nav-link href="/jobs" :active="request()->is('jobs')">
                         Jobs
                     </x-nav-link>
@@ -125,8 +122,10 @@
         </nav>
 
         <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+
+                <x-button href="/jobs/create">Create Job</x-button>
             </div>
         </header>
         <main>
