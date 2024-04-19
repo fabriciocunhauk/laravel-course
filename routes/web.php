6d8278628c44249e22c56316c7f6651591c6536e
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2024-04-19 18:05:09 +0100

    Make a Login and Registration System From Scratch: Part 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 // use Illuminate\Support\Arr;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', "home");
@@ -9,3 +11,10 @@ Route::view('/about', "about");
 Route::view('/contact', "contact");
 
 Route::resource('/jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
